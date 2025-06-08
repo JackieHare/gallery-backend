e2c85e6f10ee9deee3aa5ac8c805ccd12697840e
JackieHare <metaslender@gmail.com>
2025-06-08 17:27:27 +0200

    dodanie zdjęć

--- sql/gallery_db.sql
+++ sql/gallery_db.sql
@@ -20,10 +20,11 @@ CREATE TABLE IF NOT EXISTS artworks (
 
 -- Przykładowe dane
 INSERT INTO rooms (name, description) VALUES
-('room-1', 'Sala impresjonistów'),
-('room-2', 'Sala sztuki współczesnej');
+('hall-1', 'Sala impresjonistów'),
+('hall-2', 'Sala sztuki współczesnej');
 
 INSERT INTO artworks (room_id, title, description, image_path) VALUES
-(1, 'Impresja, wschód słońca', 'Claude Monet, 1872', 'monet1.png'),
-(1, 'Lilie wodne', 'Claude Monet, 1906', 'monet2.png'),
-(2, 'Kompozycja VIII', 'Wassily Kandinsky, 1923', 'kandinsky.png');
+(1, 'Bawół', 'Madzia R, 2021', 'bawol.jpg'),
+(1, 'Biurko Myśliciela', 'Madzia R, 2021', 'czaszka.jpg'),
+(2, 'Groszek', 'Madzia R, 2021', 'piesg.jpg'),
+;
